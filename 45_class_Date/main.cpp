9a030aebd13b73ba390e6196ccc3336e14cebb1e
Alexey Elfimov <elfimov@npfratex.ru>
2024-10-08 16:50:17 +0300

    Update 45

--- 45_class_Date/main.cpp
+++ 45_class_Date/main.cpp
@@ -1,5 +1,5 @@
-#include <chrono>
 #include <iostream>
+#include <tuple>
 
 constexpr int DEFAULT_DATE_DAY = 1;
 constexpr int DEFAULT_DATE_MONTH = 1;
@@ -44,60 +44,39 @@ int getDayOnYear(int year)
 class Date
 {
 public:
-   Date(const int day, const int month, const int year) : m_day(day), m_month(month), m_year(year)
+   Date(int day, int month, int year)
    {
-      if (!IsCorrectDate())
+      if (!IsCorrectDate(day, month, year))
       {
-         m_year = DEFAULT_DATE_YEAR;
-         m_month = DEFAULT_DATE_MONTH;
-         m_day = DEFAULT_DATE_DAY;
+         day = 0;
+      }
+      else
+      {
+         m_day = 0;
+         for (int i = DEFAULT_DATE_YEAR; i < year; ++i)
+         {
+            m_day += getDayOnYear(i);
+         }
+         m_day += DaysPassedToMonth(month, year) + day;
       }
    }
 
-   int GetYear() const { return m_year; }
-   int GetMonth() const { return m_month; }
-   int GetDay() const { return m_day; }
+   int GetYear() const { return std::get<2>(this->ToDateTuple()); }
+   int GetMonth() const { return std::get<1>(this->ToDateTuple()); }
+   int GetDay() const { return std::get<0>(this->ToDateTuple()); }
 
    Date operator+ (int d) const
    {
-      d = this->GetTotalDays() + d;
-      int month = DEFAULT_DATE_MONTH;
-      int year = DEFAULT_DATE_YEAR;
-
-      while (d > getDayOnYear(year))
-      {
-         d -= getDayOnYear(year);
-         ++year;
-      }
-
-      while (d > getDayOnMonth(month, year))
-      {
-         d -= getDayOnMonth(month, year);
-         ++month;
-      }
-
-      return Date{d, month, year};
+      Date result(*this);
+      result.SetFromDays(result.GetTotalDays() + d);
+      return result;
    }
 
    Date operator- (int d) const
    {
-      d = this->GetTotalDays() - d;
-      int month = DEFAULT_DATE_MONTH;
-      int year = DEFAULT_DATE_YEAR;
-
-      while (d > getDayOnYear(year))
-      {
-         d -= getDayOnYear(year);
-         ++year;
-      }
-
-      while (d > getDayOnMonth(month, year))
-      {
-         d -= getDayOnMonth(month, year);
-         ++month;
-      }
-
-      return Date{d, month, year};
+      Date result(*this);
+      result.SetFromDays(result.GetTotalDays() - d);
+      return result;
    }
 
    int operator- (const Date& rhs) const
@@ -105,41 +84,76 @@ public:
       return GetTotalDays() - rhs.GetTotalDays();
    }
 private:
+   int m_day{0};
+   // int m_month{DEFAULT_DATE_MONTH};
+   // int m_year{DEFAULT_DATE_YEAR};
+
+   bool IsCorrectDate(const int day, const int month, const int year) const {
+      return month <= 12 && month >= 1
+             && day <= getDayOnMonth(month, year)
+             && day > 0;
+   }
+
    bool IsCorrectDate() const {
-      return GetMonth() <= 12 && GetMonth() >= 1
-             && GetDay() <= getDayOnMonth(GetMonth(), GetYear())
-             && GetDay() > 0;
+      return IsCorrectDate(GetDay(), GetMonth(), GetYear());
    }
 
    int GetTotalDays() const
    {
-      int totalDays = 0;
-      for (int i = DEFAULT_DATE_YEAR; i < this->GetYear(); ++i)
-      {
-         totalDays += 365;
-         if (isLeapYear(i))
-            totalDays += 1;
-      }
-
-      for (int i = 1; i < this->GetMonth(); ++i)
-      {
-         totalDays += getDayOnMonth(i, this->GetYear());
-      }
-
-      totalDays += this->GetDay();
-      return totalDays;
+      return m_day;
    }
 
-   int m_day{DEFAULT_DATE_DAY};
-   int m_month{DEFAULT_DATE_MONTH};
-   int m_year{DEFAULT_DATE_YEAR};
+   int DaysPassedToMonth(int month, int year) const {
+      int result = 0;
+      for (int i = 1; i < month; ++i)
+      {
+         result += getDayOnMonth(i, year);
+      }
+      return result;
+   }
+
+   std::tuple<int, int, int> ToDateTuple() const
+   {
+      auto d = m_day;
+      int month = DEFAULT_DATE_MONTH;
+      int year = DEFAULT_DATE_YEAR;
+
+      while (d > getDayOnYear(year))
+      {
+         d -= getDayOnYear(year);
+         ++year;
+      }
+
+      while (d > DaysPassedToMonth(month + 1, year))
+         ++month;
+
+      d -= DaysPassedToMonth(month, year);
+      return {d, month, year};
+   }
+
+   void SetFromDays(int days)
+   {
+      m_day = days;
+   }
 };
+
 
 int main()
 {
-   Date d(10,2,1999);
-
+   Date d(31,1,1970);
 
    std::cout << d.GetYear() << " " << d.GetMonth() << " " << d.GetDay() << std::endl;
+
+   d = d + 1;
+   std::cout << d.GetYear() << " " << d.GetMonth() << " " << d.GetDay() << std::endl;
+
+   Date d2(1,11,1970);
+   std::cout << d2.GetYear() << " " << d2.GetMonth() << " " << d2.GetDay() << std::endl;
+
+   d2 = d2 - 1;
+   std::cout << d2.GetYear() << " " << d2.GetMonth() << " " << d2.GetDay() << std::endl;
+
+   Date d3(1,1,1970);
+   std::cout << d3.GetYear() << " " << d3.GetMonth() << " " << d3.GetDay() << std::endl;
    return 0;
 }
