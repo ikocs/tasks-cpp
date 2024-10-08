7df3a1f9803ae588cfa842a0a635ae078d0638b6
Alexey Elfimov <elfimov@npfratex.ru>
2024-10-08 10:25:23 +0300

    Final 45

--- 45_class_Date/main.cpp
+++ 45_class_Date/main.cpp
@@ -1,56 +1,144 @@
+#include <chrono>
 #include <iostream>
+
+constexpr int DEFAULT_DATE_DAY = 1;
+constexpr int DEFAULT_DATE_MONTH = 1;
+constexpr int DEFAULT_DATE_YEAR = 1970;
+
+bool isLeapYear(int year)
+{
+   return (!(year % 4) && year % 100) || !(year % 400);
+}
+
+int getDayOnMonth(int month, int year)
+{
+   switch (month)
+   {
+   case 1:
+   case 3:
+   case 5:
+   case 7:
+   case 8:
+   case 10:
+   case 12:
+      return 31;
+   case 2:
+      if (isLeapYear(year)) {
+         return 29;
+      } else {
+         return 28;
+      }
+   default:
+      return 30;
+   }
+}
+
+int getDayOnYear(int year)
+{
+   if (isLeapYear(year))
+      return 366;
+
+   return 365;
+}
 
 class Date
 {
 public:
-   Date(const int day, const int month, const int year)
+   Date(const int day, const int month, const int year) : m_day(day), m_month(month), m_year(year)
    {
-      if (year >= 1970 && year <= 2099
-         && month >= 1 && month <= 12
-         && day >= 1 && day <= getDayOnMonth(month))
+      if (!IsCorrectDate())
       {
-         m_year = year;
-         m_month = month;
-         m_day = day;
+         m_year = DEFAULT_DATE_YEAR;
+         m_month = DEFAULT_DATE_MONTH;
+         m_day = DEFAULT_DATE_DAY;
       }
    }
 
    int GetYear() const { return m_year; }
    int GetMonth() const { return m_month; }
    int GetDay() const { return m_day; }
-private:
-   int m_year{1970};
-   int m_month{1};
-   int m_day{1};
 
-   int getDayOnMonth(int mount) const
+   Date operator+ (int d) const
    {
-      switch (m_month)
+      d = this->GetTotalDays() + d;
+      int month = DEFAULT_DATE_MONTH;
+      int year = DEFAULT_DATE_YEAR;
+
+      while (d > getDayOnYear(year))
       {
-      case 1:
-      case 3:
-      case 5:
-      case 7:
-      case 8:
-      case 10:
-      case 12:
-         return 31;
-      case 2:
-         if ((m_year % 400 == 0 || m_year % 100 != 0) && m_year % 4 == 0) {
-            return 29;
-         } else {
-            return 28;
-         }
-      default:
-         return 30;
+         d -= getDayOnYear(year);
+         ++year;
       }
+
+      while (d > getDayOnMonth(month, year))
+      {
+         d -= getDayOnMonth(month, year);
+         ++month;
+      }
+
+      return Date{d, month, year};
    }
 
+   Date operator- (int d) const
+   {
+      d = this->GetTotalDays() - d;
+      int month = DEFAULT_DATE_MONTH;
+      int year = DEFAULT_DATE_YEAR;
+
+      while (d > getDayOnYear(year))
+      {
+         d -= getDayOnYear(year);
+         ++year;
+      }
+
+      while (d > getDayOnMonth(month, year))
+      {
+         d -= getDayOnMonth(month, year);
+         ++month;
+      }
+
+      return Date{d, month, year};
+   }
+
+   int operator- (const Date& rhs) const
+   {
+      return GetTotalDays() - rhs.GetTotalDays();
+   }
+private:
+   bool IsCorrectDate() const {
+      return GetMonth() <= 12 && GetMonth() >= 1
+             && GetDay() <= getDayOnMonth(GetMonth(), GetYear())
+             && GetDay() > 0;
+   }
+
+   int GetTotalDays() const
+   {
+      int totalDays = 0;
+      for (int i = DEFAULT_DATE_YEAR; i < this->GetYear(); ++i)
+      {
+         totalDays += 365;
+         if (isLeapYear(i))
+            totalDays += 1;
+      }
+
+      for (int i = 1; i < this->GetMonth(); ++i)
+      {
+         totalDays += getDayOnMonth(i, this->GetYear());
+      }
+
+      totalDays += this->GetDay();
+      return totalDays;
+   }
+
+   int m_day{DEFAULT_DATE_DAY};
+   int m_month{DEFAULT_DATE_MONTH};
+   int m_year{DEFAULT_DATE_YEAR};
 };
 
 int main()
 {
-   Date d(1,2,1978);
+   Date d(10,2,1999);
+
 
    std::cout << d.GetYear() << " " << d.GetMonth() << " " << d.GetDay() << std::endl;
    return 0;
